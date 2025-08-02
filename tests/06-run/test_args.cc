ccec061bded1a76cae8957a4d606d966f6d99cd2
Jimmy Miller <jimmyhmiller@gmail.com>
2025-08-02 12:50:26 -0400

    test: fix format

--- tests/06-run/test_args.cc
+++ tests/06-run/test_args.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(int argc, char* argv[]) {
-    std::cout << "argc=" << argc << '\n';
-    for (int i = 1; i < argc; ++i) {
-        std::cout << "arg[" << i << "]=" << argv[i] << '\n';
-    }
+  std::cout << "argc=" << argc << '\n';
+  for (int i = 1; i < argc; ++i) {
+    std::cout << "arg[" << i << "]=" << argv[i] << '\n';
+  }
 }
